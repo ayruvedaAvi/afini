bb32d8dbe4100db81f372b6c42f5103a45ccadf4
Abhi <abhishek.neupane@info.com.np>
2024-11-21 15:35:52 +0545

    Design changes in the get started screen

--- lib/presentation/screens/get_started_screen.dart
+++ lib/presentation/screens/get_started_screen.dart
@@ -1,9 +1,26 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../themes/colors.dart';
 
 class GetStartedScreen extends StatelessWidget {
   const GetStartedScreen({super.key});
+
+  Widget buildRandomShapes(double directionX, double intDirectionY,
+      BoxShape shape, double width, double height, Color color) {
+    return Align(
+      alignment: AlignmentDirectional(directionX, intDirectionY),
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color,
+          shape: shape,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,38 +37,65 @@ class GetStartedScreen extends StatelessWidget {
             ],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Stack(
           children: [
-            Image.asset('assets/images/logo.png', width: 400),
-            const SizedBox(height: 24),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Navigate to the next screen
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    child: const Text('Get Started',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: cPrimaryDark,
-                          fontWeight: FontWeight.w600,
-                        )),
+            buildRandomShapes(0.5, 0.1, BoxShape.circle, 50, 50, Colors.blue),
+            buildRandomShapes(
+                -0.8, 0.3, BoxShape.rectangle, 20, 20, Colors.blue),
+            buildRandomShapes(0.2, 0.5, BoxShape.circle, 20, 20, Colors.pink),
+            buildRandomShapes(
+                0.7, -0.7, BoxShape.rectangle, 50, 50, Colors.green),
+            buildRandomShapes(
+                -0.4, -0.9, BoxShape.circle, 30, 30, Colors.yellow),
+            buildRandomShapes(
+                0.9, 0.9, BoxShape.rectangle, 30, 30, Colors.orange),
+            buildRandomShapes(
+                -0.6, 0.7, BoxShape.circle, 40, 40, Colors.purple),
+            buildRandomShapes(
+                -0.6, -0.1, BoxShape.rectangle, 25, 30, Colors.red),
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.transparent),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png', width: 350),
+                  const SizedBox(height: 24),
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Navigate to the next screen
+                            Navigator.pushNamed(context, '/signup');
+                          },
+                          child: const Text('Get Started',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: cPrimaryDark,
+                                fontWeight: FontWeight.w600,
+                              )),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Navigate to the next screen
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        child: const Text('Already have an account? Log in',
+                            style: TextStyle(color: cPrimaryLight)),
+                      ),
+                    ],
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    // Navigate to the next screen
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  child: const Text('Already have an account? Log in',
-                      style: TextStyle(color: cPrimaryLight)),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
